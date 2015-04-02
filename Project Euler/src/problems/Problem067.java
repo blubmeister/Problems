f9eaaced6eb0fb0e97cd76418fa52c8c5fd54bd5
blubmeister <jloschek@gmail.com>
2015-04-02 11:34:06 +0200

    comment

--- Project Euler/src/problems/Problem067.java
+++ Project Euler/src/problems/Problem067.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+//copy of Problem018
+
 public class Problem067 {
 
 	public static void main(String[] args) throws IOException {
